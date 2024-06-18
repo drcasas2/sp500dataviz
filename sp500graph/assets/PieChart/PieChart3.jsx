523064707e2a2b3f3026090d618d7dfc888a5d2f
drcasas2 <105305085+drcasas2@users.noreply.github.com>
2024-06-18 14:54:26 -0400

    Labels put to the sides of the pie chart. Need to anchor the text to the center of the pie chart so text doesn't align with the left and right of the window when screen size increases.

--- sp500graph/assets/PieChart/PieChart3.jsx
+++ sp500graph/assets/PieChart/PieChart3.jsx
@@ -1,173 +1,122 @@
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
-//import styles from './AreaGraph.module.css';
-import useMeasure from "react-use-measure";
-import { format, startOfYear, startOfMonth, startOfQuarter, endOfYear, endOfMonth, endOfQuarter, eachYearOfInterval, eachMonthOfInterval, eachQuarterOfInterval, isSameYear, isSameMonth, isSameQuarter } from "date-fns";
 import { motion } from "framer-motion";
 
 const PieChart = ({ height, width, yearlySectorWeights, year }) => {
-
     const margin = { top: 40, right: 40, bottom: 40, left: 40 }
 
     const [data] = useState(yearlySectorWeights);
 
-    const svgRef = useRef();
-
     const findYearData = yearlySectorWeights.find(entry => entry.Year === year);
-   console.log(findYearData);
+    console.log(findYearData);
 
-   let yearData = [];
+    let yearData = [];
 
-   if (findYearData) {
+    if (findYearData) {
         yearData = Object.entries(findYearData.Sector).map(([Sector, Value]) => ({ Sector, Value }));
-   } //Takes each sector value in the Sector Object, and creates an array of its own Sector, Value objects
-    
-   console.log(yearData);
-    //const sectorLength = yearData.map(entry => Object.keys(entry.Sector).length);
-    //console.log(sectorLength[0]);
-        if (!findYearData) return;
-        //set up svg container
-        const w = width;
-        const h = height;
-        const radius = Math.min(w, h) / 2;
-        const colorScale = d3.scaleOrdinal(['#B8B8B8','#007CBE','#2748A5','#7086FF','#D7F3DF','#2A2B2E','#33B8FF','#99DBFF','#3961D0','#1B3374','#000F66','#031A6B']);
-        // const svg = d3.select(svgRef.current)
-        //     .attr('width', '100%')
-        //     .attr('height', '100%')
-        //     .attr('viewBox', `0 0 ${w + margin.left + margin.right} ${h + margin.top + margin.bottom}`)
-        //     .style('overflow', 'hidden')
-        //     .style('margin-top', margin.top)
-        //     .style('margin-left', 'auto')
-        //     .style('margin-right', 'auto')
-        //     .style("margin-bottom", margin.bottom);
-        //     //.style('margin', 'auto');
+    }
 
-        //     svg.selectAll("*").remove();
-        //set up chart
+    if (!findYearData) return;
 
-        //console.log(yearData.map(d => Object.values(d.Sector)))
-        //The below line of code invokes the pie method in d3, then tells d3 how to extract
-        //the values you want to use in the pie chart by grabbing the values in the object's
-        //embedded Sector object, and then (yearData) tells d3 what data you want to use
-        //to perform the operation
-        const pie = d3.pie().value(d => d.Value);
-        console.log(pie);
-        const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
-        const arcs = pie(yearData);
-        console.log(arcs);
+    const w = width;
+    const h = height;
+    const radius = Math.min(w, h) / 2;
+    const colorScale = d3.scaleOrdinal(['#B8B8B8','#007CBE','#2748A5','#7086FF','#D7F3DF','#2A2B2E','#33B8FF','#99DBFF','#3961D0','#1B3374','#000F66','#031A6B']);
+    const pie = d3.pie().value(d => d.Value);
+    const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
+    const arcs = pie(yearData);
 
-        const repositionLabels = (labels) => {
-            for (let i = 0; i < labels.length; i++) {
-                for (let j = i + 1; j < labels.length; j++) {
-                    const labelA = labels[i];
-                    const labelB = labels[j];
-                    labelA.y += 5;
-                    if (Math.abs(labelA.x - labelB.x) < 15 || Math.abs(labelA.y - labelB.y) < 15) {
-                        labelB.y += -18;
-                        labelB.x += 10;
-                    }
-                }
-            }
-            return labels;
-        };
-    
-        const labels = arcs.map((arc, i) => {
-            const [x, y] = arcGenerator.centroid(arc).map(coord => coord * 2.5);
-            return { x, y, Sector: arc.data.Sector, Value: arc.data.Value, color: colorScale(arc.index) };
-        });
-    
-        const adjustedLabels = repositionLabels(labels);
-        //set up svg data
-        
-        // const arcGroup = svg
-        //     .append("g")
-        //     .attr("transform", `translate(${(w + margin.left + margin.right)/2}, ${(h + margin.top + margin.bottom)/2})`);
+    const labels = arcs.map((arc) => {
+        const [x, y] = arcGenerator.centroid(arc).map(coord => coord * 2.5);
+        return { x, y, Sector: arc.data.Sector, Value: arc.data.Value, color: colorScale(arc.index) };
+    });
 
-        // arcGroup
-        //     .selectAll("path")
-        //     .data(arcs)
-        //     .join('path')
-        //     .attr('d', arcGenerator)
-        //     .attr('fill', (d,i) => color(i))
-        //     .style('opacity', 0.7);
+    const leftLabels = labels.filter(label => label.x < 0).sort((a, b) => a.y - b.y);
+    const rightLabels = labels.filter(label => label.x >= 0).sort((a, b) => a.y - b.y);
 
-        // //set up annotation
-        // arcGroup
-        //     .selectAll("text")
-        //     .data(arcs)
-        //     .join('text')
-        //     .text(d => `${d.data.Sector}: ${d.data.Value}%`)
-        //     .attr('transform', d => `translate(${arcGenerator.centroid(d)}), rotate(40 ${0},${0})`)
-        //     .style('text-anchor', 'middle')
-        //     .attr('class', 'text-xs');
-
+    const textHeight = 20; // Height of each text line
+    const leftLabelStartY = (h - leftLabels.length * textHeight) / 2 + margin.top;
+    const rightLabelStartY = (h - rightLabels.length * textHeight) / 2 + margin.top;
 
     console.log(yearlySectorWeights);
 
     return (
     <>
         {findYearData ? (
-                <svg
-                    className="fill-current py-5 overflow-visible"
-                    width='100%'
-                    height='100%'
-                    viewBox={`0 0 ${w + margin.left + margin.right} ${h + margin.top + margin.bottom}`}
-                    style={{ display: 'block', margin: '0 auto' }}
-                    // onMouseMove={handleMouseMove}
-                    // onMouseLeave = {handleMouseLeave}
-                >
-                            {arcs.map((arc, i) => {
-                                return (
-                                    <motion.g
-                                    transform = {`translate(${(w + margin.left + margin.right) / 2}, ${(h + margin.top + margin.bottom) / 2})`}
-                                    className="fill-current" 
-                                    key={arc.index}
-                                    initial="hidden"
-                                    animate="visible"
-                                    variants={{
-                                        visible: {
-                                            transition: {
-                                                staggerChildren: 0.2
-                                            }
-                                        }
-                                    }}
-                                    >
-                                        <motion.path
-                                            key={arc.index}
-                                            d={arcGenerator(arc)}
-                                            fill={colorScale(arc.index)}
-                                            stroke="white"
-                                            strokeWidth="3"
-                                            initial={{
-                                                opacity: 0,
-                                                pathLength: 0
-                                            }}
-                                            animate={{
-                                                opacity: 1,
-                                                pathLength: 1
-                                            }}
-                                            transition={{
-                                                duration: 1,
-                                                delay: arc.index * 0.1,
-                                            }}
-                                        />
-                                        <text
-                                            transform={`translate(${adjustedLabels[i].x}, ${adjustedLabels[i].y})`}
-                                            textAnchor="middle"
-                                            className="text-inherit mix-blend-multiply stroke-black stroke-[0.2px]"
-                                            y='0em'
-                                            dy="0em"
-                                            style={{ fontSize: '10px', fill: colorScale(arc.index), fontWeight: 'bold'}}
-                                        >
-                                            {adjustedLabels[i].Sector}: {adjustedLabels[i].Value}%
-                                        </text>
-                                        {console.log(arc)}
-                                    </motion.g>
-                            )})}
-                </ svg>
-            ) : (
-                <h1>No Sector Data Found for that Year...</h1>
-            )}
+            <svg
+                className="fill-current py-5 overflow-visible"
+                width='auto'
+                height='100%'
+                viewBox={`0 0 ${w + margin.left + margin.right} ${h + margin.top + margin.bottom}`}
+                style={{ display: 'block', margin: '0 auto' }}
+            >
+                <g transform={`translate(${(w + margin.left + margin.right) / 2}, ${(h + margin.top + margin.bottom) / 2})`}>
+                    {arcs.map((arc, i) => (
+                        <motion.g
+                            className="fill-current"
+                            key={arc.index}
+                            initial="hidden"
+                            animate="visible"
+                            variants={{
+                                visible: {
+                                    transition: {
+                                        staggerChildren: 0.2
+                                    }
+                                }
+                            }}
+                        >
+                            <motion.path
+                                key={arc.index}
+                                d={arcGenerator(arc)}
+                                fill={colorScale(arc.index)}
+                                stroke="white"
+                                strokeWidth="3"
+                                initial={{
+                                    opacity: 0,
+                                    pathLength: 0
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    pathLength: 1
+                                }}
+                                transition={{
+                                    duration: 1,
+                                    delay: arc.index * 0.1,
+                                }}
+                            />
+                        </motion.g>
+                    ))}
+                </g>
+                <g className="labels left-labels" transform={`translate(${margin.left}, ${leftLabelStartY})`}>
+                    {leftLabels.map((label, i) => (
+                        <text
+                            key={i}
+                            x={120}
+                            y={i * textHeight}
+                            textAnchor="end"
+                            style={{ fontSize: '10px', fill: label.color, fontWeight: 'bold' }}
+                        >
+                            {label.Sector}: {label.Value}%
+                        </text>
+                    ))}
+                </g>
+                <g className="labels right-labels" transform={`translate(${w + margin.right}, ${rightLabelStartY})`}>
+                    {rightLabels.map((label, i) => (
+                        <text
+                            key={i}
+                            x={30}
+                            y={i * textHeight}
+                            textAnchor="end"
+                            style={{ fontSize: '10px', fill: label.color, fontWeight: 'bold' }}
+                        >
+                            {label.Sector}: {label.Value}%
+                        </text>
+                    ))}
+                </g>
+            </svg>
+        ) : (
+            <h1>No Sector Data Found for that Year...</h1>
+        )}
     </>
     );
 };
