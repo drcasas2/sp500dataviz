84b737cd89d4529dfb82ba15b0d9d0833cc2b74b
drcasas2 <105305085+drcasas2@users.noreply.github.com>
2024-07-06 22:54:51 -0400

    Created Bars, Added Values to each Bar in the Bar Chart, and positioned them relative to negative or positive y-scaled bars using ternary operator. Need to work on Text spacing and centering the text in the bars

--- sp500graph/assets/BarChart/BarChart.jsx
+++ sp500graph/assets/BarChart/BarChart.jsx
@@ -15,7 +15,9 @@ const BarChart = ({ height, width, barData }) => {
 
     let xScale = d3.scaleBand()
         .domain(hasData ? barData.map(d => d.year) : [])
-        .range([0, graphWidth]);
+        .range([0, graphWidth])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
 
     console.log(d3.extent(barData, d => d.RoR))
 
@@ -31,6 +33,8 @@ const BarChart = ({ height, width, barData }) => {
     // console.log("yScale domain:", yScale.domain());
     // console.log("yScale range:", yScale.range());
     // console.log("yScale(0):", yScale(0));
+    console.log('xScale(d.RoR)' + barData.map( (d, i) => `Key: ${i} - (${xScale(d.year)}, ${yScale(d.RoR)})`));
+    // console.log('xScale.bandwidth(): ' + xScale.bandwidth())
 
     
 
@@ -43,26 +47,7 @@ const BarChart = ({ height, width, barData }) => {
                 {/* Setting the start drawing within the graph's margins, and ensuring data is available to display in the visualization */}
                 {hasData && (
                 <g transform={`translate(${margin.left},${margin.top})`}>
-                        {/* X-Axis */}
-                        <g transform={`translate(0,${yScale(0)})`}>
-                            <line x1={0} x2={graphWidth} className='stroke-slate-900' />
-                            {xScale.domain().map((d, i) => (
-                                <g key={i} transform={`translate(${xScale(d) + xScale.bandwidth() / 2},0)`}>
-                                    <line y2="6" stroke="black" />
-                                    <text
-                                        y="20"
-                                        textAnchor="middle"
-                                        className='text-sm mx-[4px] lg:text-lg xl:text-xl'
-                                    >
-                                        {d}
-                                    </text>
-                                </g>
-                            ))}
-                        </g>
-                        {/* Y-Axis */}
-                        <g transform={`translate(0, ${graphHeight})`}>
-                            <line x1={0} x2={0} y1={0} y2={-graphHeight} className='stroke-slate-900'/>
-                        </g>
+                        {/* Y-Axis Ticks and Text */}
                         {yScale.ticks().map((d, i) => (  //You need to use .ticks() in the yScale, because the y-axis data is not categorical, so some data can be displayed between the ticks. you can only directly map over the domain in the xScale, because the x-axis is considered categorical data in our data array.
                             <g key={i} transform={`translate(0, ${yScale(d)})`}>
                                 <line x1={graphWidth} x2={ -8} className='stroke-slate-600 stroke-[0.5px] stroke-dasharray-2-4' />
@@ -73,19 +58,55 @@ const BarChart = ({ height, width, barData }) => {
                                     textAnchor="end"
                                     className='text-sm'
                                 >
-                                    {d}
+                                    {`${d}%`}
                                 </text>
                             </g>
                         ))}
-
-                        {/* Trying to create the bars to draw on the graph. This is completely wrong so far, but I will work on this */}
-                        {/* {barData.map((d, i) => (
-                            <g key={i} transform={`translate(${xScale(d.RoR) + xScale.bandwidth()}, ${yScale(d.year)})`}>
-                                <rect>
-
-                                </rect>
+                        {/* Creating the Bars on the Bar Chart */}
+                        {barData.map((d, i) => (
+                            <g key = {i}>
+                                <rect
+                                    x={xScale(d.year)}
+                                    y={yScale(Math.max(0, d.RoR))}  // Ensure y is positive for negative values
+                                    width={xScale.bandwidth()}
+                                    height={Math.abs(yScale(d.RoR) - yScale(0))}  // Calculate height from zero line
+                                    className="fill-sky-500 pointer-events-none"
+                                />
+                                {/* I need to work on how to center the text within each of the bars in the bar chart without spreading out the text if the bar width gets wider as the screen gets wider */}
+                                <g>
+                                    <text
+                                        className='text-[3px] mx-[4px] sm:text-sm lg:text-lg xl:text-xl stroke-white stroke-[0.8] backdrop-invert'
+                                        x={xScale(d.year)}
+                                        y={yScale(d.RoR)}  // Ensure y is positive for negative values
+                                        dy = {yScale(d.RoR) >= 160 ? '-2' : '8'}
+                                        textLength= {xScale.bandwidth()}
+                                        lengthAdjust= 'spacing'
+                                    >
+                                    {`${Math.round(d.RoR * 10) / 10}%`}
+                                    </text>
+                                </g>
                             </g>
-                        ))} */}
+                        ))}
+                        {/* X-Axis & X-Axis Ticks & Text */}
+                        <g transform={`translate(0,${yScale(0)})`}>
+                            <line x1={0} x2={graphWidth} className='stroke-slate-900' />
+                            {xScale.domain().map((d, i) => (
+                                <g key={i} transform={`translate(${xScale(d) + xScale.bandwidth() / 2},0)`}>
+                                    <line y1="-3" y2="3" className="stroke-slate-800 stroke-[0.5px]" />
+                                    <text
+                                        y="10"
+                                        textAnchor="middle"
+                                        className='text-[5px] mx-[4px] sm:text-sm lg:text-lg xl:text-xl stroke-blue-800 stroke-[0.3] backdrop-invert'
+                                    >
+                                        {d}
+                                    </text>
+                                </g>
+                            ))}
+                        </g>
+                        {/* Y-Axis */}
+                        <g transform={`translate(0, ${graphHeight})`}>
+                            <line x1={0} x2={0} y1={0} y2={-graphHeight} className='stroke-slate-900'/>
+                        </g>
                 </g>
                 )}
             </svg>
